f16260d8f5dcb746181fd9670d5a78cf23a28da3
NatashaPletniova <pletniovan@gmail.com>
2017-02-12 15:07:10 +0300

    Task_3 was added.

---
+++ Task_3/Lesson1_3.java
@@ -0,0 +1,14 @@
+﻿package by.epam.homework.lesson1;
+
+public class Lesson1_3 {
+	
+	public static void main(String[] args){
+		
+		int x = 5678;
+		int p;
+		
+		p = (x / 1000) * ((x / 100) % 10) * ((x / 10) % 10) * (x % 10);
+		System.out.println("The result "+x+" = "+p );
+
+	}
+}
