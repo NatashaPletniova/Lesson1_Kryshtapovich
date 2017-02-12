095adc25e22994da3cb6973a2756222a0aa77da9
NatashaPletniova <pletniovan@gmail.com>
2017-02-12 13:45:25 +0300

    Task_2 the first commit.

---
+++ Task_2/Lesson1_2.java
@@ -0,0 +1,26 @@
+﻿package by.epam.homework.lesson1;
+public class Lesson1_2 {
+
+	public static void main(String[] args){
+		
+			double r = 4;
+			double s, l;
+		
+
+	        
+	        if (r > 0){
+	            l = 2 * Math.PI * r;
+	            System.out.println("Perimeter of circle = "+l );	
+	            s = Math.PI * r * r;
+	            System.out.println("Area of circle = "+s );	
+	        }
+	        else
+	        {
+	            System.out.println("Radius could not be negative or equal to zero!");
+	            return;
+	        }
+
+
+			
+		}
+}
